485d7931bab5daaefda7976bd8afe595ad0b061e
wa1d3mar <vovkakrot.com@gmail.com>
2020-09-27 21:37:27 +0300

    LetterCounter UPDATE

--- tasks/crypto_cp_1/grigoryan_fb84_biletskiy_fb84_cp1/letter_counter.py
+++ tasks/crypto_cp_1/grigoryan_fb84_biletskiy_fb84_cp1/letter_counter.py
@@ -6,21 +6,24 @@ alphabet = ('а','б','в','г','д','е','ё','ж','з','и','й','к','л','м
 buffer = ''
 sumletters = 0
 
-for i in alphabet:
-    result_file = open("filteredtext.txt",'r',encoding='utf-8')
-    print(i, result_file.read().count(i))
 
+a = input("1. letter counter / 2. letter frequency: \n")
 
-result_file = open("filteredtext.txt",'r',encoding='utf-8')
-for i in result_file:
-    for j in i:
-        if j in alphabet:
-            sumletters += 1
+if a == "1":
+    for i in alphabet:
+        result_file = open("filteredtext.txt",'r',encoding='utf-8')
+        print(i, result_file.read().count(i))
+
+if a == '2':
+    result_file = open("filteredtext.txt", 'r', encoding='utf-8')
+    for i in result_file:
+        for j in i:
+            if j in alphabet:
+                sumletters += 1
+
+    for i in alphabet:
+        result_file = open("filteredtext.txt",'r',encoding='utf-8')
+        print(i, result_file.read().count(i)/sumletters)
+
 
 print("SUM: " + str(sumletters))
-
-
-for i in result_file:
-    for j in i:
-        if j in alphabet:
-            sumletters += 1
